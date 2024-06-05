968706711342b73af6701621936381e9592892d1
Ruben Salazar <rubensalazar@rocketmail.com>
2024-06-05 16:15:45 -0500

    meth lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+
+function superbowlWin(records){
+    console.log(records)
+const foundRecord = records.find(record => record.result === "W")
+console.log(foundRecord)
+if(foundRecord === undefined){
+    
+    return undefined
+}
+return foundRecord.year
+
+}
+
+
+
+
+
+
+   
+      
+
